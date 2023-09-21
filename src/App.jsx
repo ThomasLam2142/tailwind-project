18f366c82cd966a1ce8d40cbed93c5e8eb435f48
Thomas Lam <thomaslam2142@gmail.com>
2023-09-21 18:56:52 -0400

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const App = () =>{
+  return <div>
+    <section>
+      <p>
+        Ex culpa eiusmod exercitation velit nostrud sit et voluptate sit ad aliquip laboris incididunt. 
+        Cillum fugiat esse laboris deserunt. Voluptate et labore enim quis aute ex excepteur tempor laborum. 
+        Mollit exercitation tempor sunt et id proident labore aliquip ea irure aute voluptate tempor nostrud. 
+        Amet enim occaecat nostrud commodo nulla cupidatat incididunt exercitation. 
+        Labore consequat amet minim non excepteur pariatur ipsum tempor reprehenderit eu eiusmod nisi nostrud.
+      </p>
+    </section>
+  </div>
+}
+
+export default App;
